3ccb8cc24818513749d4a60f66e7f89157c8aa8a
Norbert Ozga <norbert.ozga@cervirobotics.com>
2021-03-22 23:13:54 +0100

    Implement example views

---
+++ backend/backend_application/open_weather/views.py
@@ -0,0 +1,14 @@
+from flask import Blueprint
+
+from ..extensions import cache
+from . import daemon as dow
+
+blueprint = Blueprint("open_weather", __name__, url_prefix="/open-weather")
+
+
+@blueprint.route("/get-current", methods=["GET"])
+def get_current():
+    open_weather = dow.OpenWeather()
+    open_weather.cache_current()
+
+    return cache.get("current").decode()
