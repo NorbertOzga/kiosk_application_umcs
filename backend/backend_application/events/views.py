2a75d852337c9fd3c0d03d3bdc1ab564130b3d17
nozga <norbert.ozga@dronehub.ai>
2021-05-17 18:00:01 +0200

    Fix api and deamons

--- backend/backend_application/events/views.py
+++ backend/backend_application/events/views.py
@@ -1,13 +1,39 @@
+import datetime
 import json
 
 from flask import Blueprint
+
+from . import services
 
 from ..extensions import cache
 
 blueprint = Blueprint("events", __name__, url_prefix="/events")
 
 
+def check_cache_events(func):
+    def wrapper_events():
+        last_events_fetch = cache.get("last_events_fetch")
+        if (
+            not last_events_fetch
+            or datetime.datetime.now()
+            - datetime.datetime.strptime(
+                last_events_fetch.decode(), "%Y-%m-%d %H:%M:%S"
+            )
+            > datetime.timedelta(seconds=10)
+        ):
+            events = services.Events()
+            events.get_events()
+            cache.set(
+                "last_events_fetch",
+                datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            )
+
+        return func()
+    return wrapper_events
+
+
 @blueprint.route("/get_events", methods=["GET"])
+@check_cache_events
 def get_events():
     data = cache.get("events")
     return json.dumps(data.decode()).replace("'", '"')[1:-1]
--- backend/backend_application/instagram_post/views.py
+++ backend/backend_application/instagram_post/views.py
@@ -1,14 +1,39 @@
-import io
 import json
+import datetime
 
 from flask import Blueprint
 
+from . import services
 from ..extensions import cache
 
 blueprint = Blueprint("instagram_post", __name__, url_prefix="/instagram_post")
 
 
+def check_cache(func):
+    def wrapper_instagram():
+        last_instagram_fetch = cache.get("last_instagram_fetch")
+        if (
+            not last_instagram_fetch
+            or datetime.datetime.now()
+            - datetime.datetime.strptime(
+                last_instagram_fetch.decode(), "%Y-%m-%d %H:%M:%S"
+            )
+            > datetime.timedelta(minutes=45)
+        ):
+            insta_scraper = services.InstaScraper()
+            insta_scraper.get_instagram_data()
+            cache.set(
+                "last_instagram_fetch",
+                datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            )
+
+        return func()
+
+    return wrapper_instagram
+
+
 @blueprint.route("/get_post", methods=["GET"])
+@check_cache
 def get_posts():
     data = cache.get("instagram_posts")
     return json.dumps(data.decode()).replace("'", '"')[1:-1]
