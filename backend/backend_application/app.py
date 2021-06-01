ca4296ed2fd53094c5476e899fc227194c645a22
h-okon <hubrr.oko@gmail.com>
2021-06-01 23:16:27 +0200

    Create docker network, add phantomjs, buses support

--- backend/backend_application/app.py
+++ backend/backend_application/app.py
@@ -3,6 +3,7 @@ from flask import Flask
 from .open_weather.views import blueprint as owbp
 from .instagram_post.views import blueprint as ipbp
 from .events.views import blueprint as esbp
+from .buses.views import blueprint as bsbp
 from .settings import ConfigType, ConfigFactory
 from .extensions import scheduler
 
@@ -22,6 +23,7 @@ def register_blueprints(app: Flask) -> None:
     app.register_blueprint(owbp)
     app.register_blueprint(ipbp)
     app.register_blueprint(esbp)
+    app.register_blueprint(bsbp)
 
 
 def register_extensions(app: Flask) -> None:
